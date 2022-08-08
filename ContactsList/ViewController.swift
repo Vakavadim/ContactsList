bd6f7fca93a95d0154c7fe9388f9eb0222f2e8d3
Vadim Gamzaev <Vadosbox@gmail.com>
2022-08-08 16:05:55 +0300

    Initial Commit

---
+++ ContactsList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ContactsList
+//
+//  Created by Вадим Гамзаев on 08.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
